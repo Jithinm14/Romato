527ac3533afac3b9aebdd53b3dc12979cb2befce
Jithin M <jithinmbalu14@gmail.com>
2020-07-14 12:58:03 +0530

    Initial Commit

---
+++ Romato/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Romato
+//
+//  Created by Jithin M on 14/07/20.
+//  Copyright © 2020 Jithin M. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
