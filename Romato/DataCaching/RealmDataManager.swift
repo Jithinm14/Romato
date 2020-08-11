60fa3d5cc85c53401cce378f9a1d1bed5034b8d1
Jithin M <jithinmbalu14@gmail.com>
2020-08-11 20:46:31 +0530

    Adding directory structure

---
+++ Romato/DataCaching/RealmDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  RealmDataManager.swift
+//  Romato
+//
+//  Created by Jithin M on 31/07/20.
+//  Copyright © 2020 Jithin M. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmDataManager {
+    
+    static let shared = RealmDataManager()
+    
+    private init() {}
+    
+}
